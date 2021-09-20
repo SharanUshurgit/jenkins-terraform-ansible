ca404f30c4c983c496d1a3c53ea105ff052300b8
Sharan <89379489+SharanUshurgit@users.noreply.github.com>
2021-09-20 13:40:37 +0530

    Update installcommand.sh

--- automation/method2/installcommand.sh
+++ automation/method2/installcommand.sh
@@ -4,6 +4,6 @@
         /usr/bin/mkdir /home/ansibletest/.ssh
         /usr/bin/chown ansibletest:ansibletest /home/ansibletest/.ssh
         /usr/bin/chmod 700 /home/ansibletest/.ssh
-        echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDYlEbQhSjLSYaFiFY9hVf0KRK8AWmwBkwPIl1ir5IK+MQNhHOStjzJqOZZOoPVuVn8sEzGmt00KcX9Dm5SZhOIDdHWvgb5QtmV4Z4wHEpv97j/1vx7tmJiNAoGUlA8OEqJzdMxpQu3wetr/W+SegSbhOyrJllckmrhFxqiVR7WyD7iIuc2L3hXbJNtiMS5uelffdmAmqSNFzXDZCQZB8GT4fZ6tIOxT06S3kpyb3MvbwVhNxT/d2uZmE38XojakzXM0RjF7vPj4xB7QPj9q7Htz9JtQtv5h0U6OTfB9PUJ3UAFYXBRpISqIa1dyXl+uSsTppdac03aAOcfZRCBDTnt root@ldapmas.technix.com" >> /home/ansibletest/.ssh/authorized_keys
+        echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDipsDmXiXhLzmhE9FAuUFbrSj8enHxCHLL4uAP8C36lid/kun+Dixwvv2JE7tQmNQIZbThkFXuKGSmZr6pch8/6c15ESoJ90Sau1PBuPUq4zjaiaN068Cut9e7noxcnFqjrQxyamWoNxCVRTG4mu8c8xdNA4QIUhOj8tbKnTjQnaubBQCZ+/Sg9gDPRs+7Qylqg2gFkEs64xzj2/CLNouwRLiUNRThVieVoMfixNb3/hGMOO+xaQROMVj12K3T7YiRznV38ntL+QmoUVo7PKlJXx/cBRqBfyfklV/KxMvt1eBY5wWK49QcKZse8LzIC0LvIMWitId6mNRheRNsow5l Jenkins" >> /home/ansibletest/.ssh/authorized_keys
         /usr/bin/chmod 600 /home/ansibletest/.ssh/authorized_keys
         /usr/bin/chown ansibletest:ansibletest /home/ansibletest/.ssh/authorized_keys
