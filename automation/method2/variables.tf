da602e9d93ef16efe601ead013f90a81465a6339
Sharan <89379489+SharanUshurgit@users.noreply.github.com>
2021-09-22 19:17:45 +0530

    Update variables.tf

--- automation/method2/variables.tf
+++ automation/method2/variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
     description = "Name of security group"
-    default     = "my-jenkins-security-group7"
+    default     = "Test_SG"
 }
 
 variable "tag_name" {
